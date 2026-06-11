4cd18182c32741e1b4a9a1c2a12fcec1301ca590
dmagich <dmitryworkhost@gmail.com>
2026-06-11 20:31:30 +0300

    feat: Added basic echo-response to bot

---
+++ TelegramBotYtMusic/Worker.cs
@@ -0,0 +1,62 @@
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace TelegramBotYtMusic;
+
+public class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    private readonly TelegramBotClient _botClient = new("");
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = Array.Empty<UpdateType>() 
+        };
+
+        logger.LogInformation("Запуск бота...");
+
+        _botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            errorHandler: HandleErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: stoppingToken
+        );
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+    private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        if (update.Message is not { } message) return;
+        if (message.Text is not { } messageText) return;
+
+        var chatId = message.Chat.Id;
+
+        logger.LogInformation($"Получено сообщение '{messageText}' в чате {chatId}.");
+
+        await botClient.SendMessage(
+            chatId: chatId,
+            text: "Привет! Ты написал: " + messageText,
+            cancellationToken: cancellationToken);
+    }
+
+    private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        var errorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Ошибка Telegram API:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        logger.LogError(errorMessage);
+        return Task.CompletedTask;
+    }
+}
